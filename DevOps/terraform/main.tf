e20bcc4555e501c950aad3c104a7f75058ae58fb
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-24 15:49:24 +0700

    Fix terraform

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_function_app" "example" {
 
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  service_plan_id            = azurerm_service_plan.example.id
+  app_service_plan_id            = azurerm_service_plan.example.id
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "dotnet-isolated",
