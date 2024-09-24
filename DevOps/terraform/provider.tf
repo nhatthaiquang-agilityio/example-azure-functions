0e26b6907911771c1dbbf54a94a0f02c7d85e8eb
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-24 15:32:39 +0700

    Remove resource

--- DevOps/terraform/provider.tf
+++ DevOps/terraform/provider.tf
@@ -7,11 +7,6 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.71"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.71"
-    }
   }
 
   # If a configuration includes no backend block, Terraform defaults to using the local backend, which stores state as a plain file in the current working directory.
