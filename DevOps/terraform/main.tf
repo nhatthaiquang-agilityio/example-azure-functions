e3b4cf20f73a3af900f5b028b6773c7dea038b6e
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-24 17:01:01 +0700

    set access key

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -35,6 +35,7 @@ resource "azurerm_windows_function_app" "example" {
   location            = var.location
 
   storage_account_name = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
   service_plan_id      = azurerm_service_plan.example.id
 
   app_settings = {
